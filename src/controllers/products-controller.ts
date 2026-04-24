81f47cebdbe1786fefb0e29dd8286ce007094b81
Matheus Silva <rematheussilva@gmail.com>
2026-04-24 13:57:34 -0500

    feat: add create product endpoint

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,9 +1,23 @@
 import { NextFunction, Request, Response } from "express";
+import { z } from "zod";
 
 export class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
       return response.json({ message: "Ok!" });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(5),
+        price: z.number().gt(0, { message: "value must be grater than 0" }),
+      });
+
+      const { name, price } = bodySchema.parse(request.body);
+      return response.status(200).json({ name, price });
     } catch (error) {
       next(error);
     }
