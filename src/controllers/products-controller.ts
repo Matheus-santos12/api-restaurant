665011874ef8773dfe6adfe479267b7a6d93cc25
Matheus Silva <rematheussilva@gmail.com>
2026-04-27 15:10:10 -0500

    feat: add remove product endpoint

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,4 +1,5 @@
 import { knex } from "@/database/knex";
+import { AppError } from "@/utils/AppError";
 import { NextFunction, Request, Response } from "express";
 import { z } from "zod";
 
@@ -54,6 +55,33 @@ export class ProductController {
       await knex<ProductRepository>("products")
         .update({ name, price, updated_at: knex.fn.now() })
         .where({ id });
+
+      return response.json();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async remove(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), {
+          message: "id must be a number",
+        })
+        .parse(request.params.id);
+
+      const product = await knex<ProductRepository>("products")
+        .select()
+        .where({ id })
+        .first();
+
+      if (!product) {
+        throw new AppError("product Not found");
+      }
+
+      await knex<ProductRepository>("products").delete().where({ id });
 
       return response.json();
     } catch (error) {
