c2bb7ecbbb68f1715f06bab132d54028dd8539c4
Matheus Silva <rematheussilva@gmail.com>
2026-04-29 08:24:07 -0500

    feat: add create tables sessions endpoint scaffold

---
+++ src/controllers/tables-sessions-controller.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+
+export class TablesSessionsController {
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      return response.status(201).json();
+    } catch (error) {
+      next(error);
+    }
+  }
+}
