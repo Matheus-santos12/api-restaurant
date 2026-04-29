49866db177837e35112d31acdf999064cdd5e964
Matheus Silva <rematheussilva@gmail.com>
2026-04-28 22:45:16 -0500

    feat: add list tables enpoint

---
+++ src/controllers/tables-controller.ts
@@ -0,0 +1,16 @@
+import { knex } from "@/database/knex";
+import { NextFunction, Request, Response } from "express";
+
+export class TablesController {
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      const tables = await knex<TableRepository>("tables")
+        .select()
+        .orderBy("table_number");
+
+      return response.json(tables);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
